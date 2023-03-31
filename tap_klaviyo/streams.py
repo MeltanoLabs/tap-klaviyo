c6d0948829e861fb0de1a16be56f248a2d55c426
Leopoldo Araujo <68853890+poloaraujo@users.noreply.github.com>
2023-03-31 15:47:42 +0200

    Updated streams

--- tap_klaviyo/streams.py
+++ tap_klaviyo/streams.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 from pathlib import Path
+from datetime import datetime
 
 from singer_sdk import typing as th  # JSON Schema typing helpers
 
@@ -51,75 +52,75 @@ class EventsStream(KlaviyoStream):
         row["datetime"] = row["attributes"]["datetime"]
         return row
 
-
 class CampaignsStream(KlaviyoStream):
     """Define custom stream."""
 
     name = "campaigns"
     path = "/campaigns"
     primary_keys = ["id"]
-    replication_key = None
+    replication_key = "updated_at"
+    schema_filepath = SCHEMAS_DIR / "campaigns.json"
 
-    schema = th.PropertiesList(
-        th.Property(
-            "id",
-            th.StringType,
-            description="The campaign ID"),
-        th.Property(
-            "attributes",
-            th.ObjectType(
-                th.Property("name",
-                    th.StringType,
-                    description="The campaign name"
-                ),
-                th.Property(
-                    "type",
-                    th.StringType,
-                    description="The campaign type"
-                ),
-                th.Property(
-                    "status",
-                    th.StringType,
-                    description="The campaign status"
-                ),
-                th.Property(
-                    "archived",
-                    th.BooleanType,
-                    description="Whether the campaign has been archived or not"
-                ),
-                th.Property(
-                    "channel",
-                    th.StringType,
-                    description="The campaign channel"
-                ),
-                th.Property(
-                    "message",
-                    th.StringType,
-                    description="The campaign message (id?)"
-                ), #TODO: validate
-                th.Property(
-                    "created_at",
-                    th.DateTimeType,
-                    description="Timestamp when the campaign was created"
-                ),
-                th.Property(
-                    "scheduled_at",
-                    th.DateTimeType,
-                    description="Timestamp when the campaign was scheduled"
-                ),
-                th.Property(
-                    "updated_at",
-                    th.DateTimeType,
-                    description="Timestamp when the campaign was updated"
-                ),
-                th.Property(
-                    "send_time",
-                    th.StringType,
-                    description="The campaign time when the campaign was sent"
-                ),
-            ),
-        ),
-    ).to_dict()
+    # schema = th.PropertiesList(
+    #     th.Property(
+    #         "id",
+    #         th.StringType,
+    #         description="The campaign ID"),
+    #     th.Property(
+    #         "attributes",
+    #         th.ObjectType(
+    #             th.Property("name",
+    #                 th.StringType,
+    #                 description="The campaign name"
+    #             ),
+    #             th.Property(
+    #                 "type",
+    #                 th.StringType,
+    #                 description="The campaign type"
+    #             ),
+    #             th.Property(
+    #                 "status",
+    #                 th.StringType,
+    #                 description="The campaign status"
+    #             ),
+    #             th.Property(
+    #                 "archived",
+    #                 th.BooleanType,
+    #                 description="Whether the campaign has been archived or not"
+    #             ),
+    #             th.Property(
+    #                 "channel",
+    #                 th.StringType,
+    #                 description="The campaign channel"
+    #             ),
+    #             th.Property(
+    #                 "message",
+    #                 th.StringType,
+    #                 description="The campaign message (id?)"
+    #             ), #TODO: validate
+    #             th.Property(
+    #                 "created_at",
+    #                 th.DateTimeType,
+    #                 description="Timestamp when the campaign was created"
+    #             ),
+    #             th.Property(
+    #                 "scheduled_at",
+    #                 th.DateTimeType,
+    #                 description="Timestamp when the campaign was scheduled"
+    #             ),
+    #             th.Property(
+    #                 "updated_at",
+    #                 th.DateTimeType,
+    #                 description="Timestamp when the campaign was updated"
+    #             ),
+    #             th.Property(
+    #                 "send_time",
+    #                 th.StringType,
+    #                 description="The campaign time when the campaign was sent"
+    #             ),
+    #         ),
+    #     ),
+    # ).to_dict()
 
     def get_url_params(
         self,
@@ -135,6 +136,7 @@ class CampaignsStream(KlaviyoStream):
         Returns:
             A dictionary of URL query parameters.
         """
+
         params: dict = {
             **self.base_url_params,
         }
@@ -142,11 +144,21 @@ class CampaignsStream(KlaviyoStream):
         if next_page_token:
             params["page[cursor]"] = next_page_token
 
-        # TODO: Convert this to a config var
-        # If the following line gets commented out, the tap will fail
-        #params["filter"] = "greater-than(datetime,2023-02-22T00:00:00Z)"
+        if self.replication_key:
+            if self.get_starting_timestamp(context):
+                filter_timestamp = self.get_starting_timestamp(context)
+            elif self.config.get("start_date"):
+                filter_timestamp = datetime.strptime(self.config("start_date"), "%Y-%m-%d").isoformat()
+            else:
+                filter_timestamp = datetime(2000,1,1).isoformat()
+
+            params["filter"] = f"greater-than({self.replication_key},{filter_timestamp})"
 
         return params
+
+    def post_process(self, row: dict, context: dict | None = None) -> dict | None:
+        row["updated_at"] = row["attributes"]["updated_at"]
+        return row
 
 class MetricsStream(KlaviyoStream):
     """Define custom stream."""
@@ -154,43 +166,44 @@ class MetricsStream(KlaviyoStream):
     name = "metrics"
     path = "/metrics"
     primary_keys = ["id"]
-    replication_key = None
+    replication_key = "created"
+    schema_filepath = SCHEMAS_DIR / "metrics.json"
 
-    schema = th.PropertiesList(
-        th.Property(
-            "id",
-            th.StringType,
-            description="The metric ID"),
-        th.Property(
-            "type",
-            th.StringType,
-            description="The metric type"),
-        th.Property(
-            "attributes",
-            th.ObjectType(
-                th.Property(
-                    "name",
-                    th.StringType,
-                    description="The metric name"
-                ),
-                th.Property(
-                    "created",
-                    th.StringType,
-                    description="Timestamp when the metric was created"
-                ),
-                th.Property( # TODO: This needs to be renamed?
-                    "integration",
-                    th.ObjectType(
-                        th.Property(
-                            "id",
-                            th.StringType,
-                            description="The name of the integration"
-                        )
-                    ),
-                ),
-            ),
-        ),
-    ).to_dict()
+    # schema = th.PropertiesList(
+    #     th.Property(
+    #         "id",
+    #         th.StringType,
+    #         description="The metric ID"),
+    #     th.Property(
+    #         "type",
+    #         th.StringType,
+    #         description="The metric type"),
+    #     th.Property(
+    #         "attributes",
+    #         th.ObjectType(
+    #             th.Property(
+    #                 "name",
+    #                 th.StringType,
+    #                 description="The metric name"
+    #             ),
+    #             th.Property(
+    #                 "created",
+    #                 th.StringType,
+    #                 description="Timestamp when the metric was created"
+    #             ),
+    #             th.Property(
+    #                 "integration",
+    #                 th.ObjectType(
+    #                     th.Property(
+    #                         "id",
+    #                         th.StringType,
+    #                         description="The name of the integration"
+    #                     )
+    #                 ),
+    #             ),
+    #         ),
+    #     ),
+    # ).to_dict()
 
     def get_url_params(
         self,
@@ -206,6 +219,7 @@ class MetricsStream(KlaviyoStream):
         Returns:
             A dictionary of URL query parameters.
         """
+
         params: dict = {
             **self.base_url_params,
         }
@@ -213,8 +227,12 @@ class MetricsStream(KlaviyoStream):
         if next_page_token:
             params["page[cursor]"] = next_page_token
 
-        # TODO: Convert this to a config var
-        # If the following line gets commented out, the tap will fail
-        #params["filter"] = "greater-than(datetime,2023-02-22T00:00:00Z)"
+        if self.replication_key:
+            filter_timestamp = self.get_starting_timestamp(context)
+            params["filter"] = f"greater-than(created,{filter_timestamp})"
 
         return params
+
+    def post_process(self, row: dict, context: dict | None = None) -> dict | None:
+        row["created"] = row["attributes"]["created"]
+        return row
