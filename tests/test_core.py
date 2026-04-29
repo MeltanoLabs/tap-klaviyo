d844b95127dc5e9c7a473a8520c02a6332e122cc
msahu-meltano <msahu@meltano.com>
2026-04-29 16:57:30 +0530

    MEL-298 Fix listperson composite primary key

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,7 +1,5 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
-from unittest import TestCase
-
 from singer_sdk.testing import get_tap_test_class
 
 from tap_klaviyo import streams
@@ -19,28 +17,20 @@ TestTapKlaviyo = get_tap_test_class(
 )
 
 
-class TestPrimaryKeys(TestCase):
+def test_relationship_and_series_report_primary_keys() -> None:
     """Validate primary keys that prevent merge collisions."""
-
-    def test_relationship_and_series_report_primary_keys(self) -> None:
-        self.assertEqual(streams.ListPersonStream.primary_keys, ["list_id", "id"])
-        self.assertEqual(
-            streams.SegmentSeriesReportStream.primary_keys,
-            [
-                "report_name",
-                "segment_id",
-                "date",
-                "statistic_name",
-            ],
-        )
-        self.assertEqual(
-            streams.FlowSeriesReportStream.primary_keys,
-            [
-                "report_name",
-                "flow_id",
-                "flow_message_id",
-                "send_channel",
-                "date",
-                "statistic_name",
-            ],
-        )
+    assert streams.ListPersonStream.primary_keys == ["list_id", "id"]  # noqa: S101
+    assert streams.SegmentSeriesReportStream.primary_keys == [  # noqa: S101
+        "report_name",
+        "segment_id",
+        "date",
+        "statistic_name",
+    ]
+    assert streams.FlowSeriesReportStream.primary_keys == [  # noqa: S101
+        "report_name",
+        "flow_id",
+        "flow_message_id",
+        "send_channel",
+        "date",
+        "statistic_name",
+    ]
