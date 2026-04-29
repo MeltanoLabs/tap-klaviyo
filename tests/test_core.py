1bbd4344246612c58d280f6e1b80bba35e26789f
msahu-meltano <msahu@meltano.com>
2026-04-29 19:06:19 +0530

    MEL-298 test code removed

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,7 +2,6 @@
 
 from singer_sdk.testing import get_tap_test_class
 
-from tap_klaviyo import streams
 from tap_klaviyo.tap import TapKlaviyo
 
 SAMPLE_CONFIG = {
@@ -15,22 +14,3 @@ TestTapKlaviyo = get_tap_test_class(
     tap_class=TapKlaviyo,
     config=SAMPLE_CONFIG,
 )
-
-
-def test_relationship_and_series_report_primary_keys() -> None:
-    """Validate primary keys that prevent merge collisions."""
-    assert streams.ListPersonStream.primary_keys == ["list_id", "id"]  # noqa: S101
-    assert streams.SegmentSeriesReportStream.primary_keys == [  # noqa: S101
-        "report_name",
-        "segment_id",
-        "date",
-        "statistic_name",
-    ]
-    assert streams.FlowSeriesReportStream.primary_keys == [  # noqa: S101
-        "report_name",
-        "flow_id",
-        "flow_message_id",
-        "send_channel",
-        "date",
-        "statistic_name",
-    ]
